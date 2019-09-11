6a64642de7236678abb8b7170cfbd0fd17d70419
Cameron Lund <cameronlund4@gmail.com>
2019-09-10 20:45:06 -0400

    Minor math fixes and added new ways to calc cubes

--- src/App.js
+++ src/App.js
@@ -10,12 +10,12 @@ class App extends Component {
     super(props);
 
     this.state = {
-      cubesRed: 0,
+      cubesRed: 1,
       cubesRedChecked: true,
       selFactorRed: .25,
       selFactorRedChecked: false,
       towerSelChecked: false,
-      cubesBlue: 0,
+      cubesBlue: 1,
       selFactorBlue: 1,
       towerSel: 1
     }
@@ -23,7 +23,20 @@ class App extends Component {
   }
 
   render() {
-    var result = (this.state.cubesRed + (this.state.selFactorRed * (this.state.cubesRed * (this.state.towerSel - 1)))) - (this.state.cubesBlue + (this.state.selFactorBlue * (this.state.cubesBlue * (this.state.towerSel - 1))));
+    var towerSel = this.state.towerSel;
+    var selFactorRed = this.state.selFactorRed
+    var selFactorBlue = this.state.selFactorBlue
+    if (towerSel < 0) {
+      towerSel = 0;
+      selFactorRed = 0;
+      selFactorBlue = 0;
+    }
+      
+    var result = (this.state.cubesRed + Math.floor(selFactorRed * (this.state.cubesRed * (towerSel)))) - (this.state.cubesBlue + Math.floor(selFactorBlue * (this.state.cubesBlue * (towerSel))));
+    console.log((this.state.cubesRed + (selFactorRed * (this.state.cubesRed * (towerSel)))))
+    console.log((this.state.cubesBlue + (selFactorBlue * (this.state.cubesBlue * (towerSel)))))
+    console.log(this.state.cubesBlue);
+    console.log((selFactorBlue * (this.state.cubesBlue * (towerSel))))
 
     return (
       <div style={{
@@ -40,32 +53,74 @@ class App extends Component {
         <div className="container-fluid">
           <div className="row justify-content-md-center">
             <div className="col-sm-6">
-              <EquationVariable
-                value={this.state.cubesRed}
-                checkCallback={(title, checked) => {
-                  this.setState({ cubesRedChecked: true, selFactorRedChecked: false, towerSelChecked: false })
-                }}
-                valueCallback={(title, cubesRed) => {
-                  this.setState({ cubesRed })
-                }}
-                checkable={true}
-                checked={this.state.cubesRedChecked}
-                label="Red cubes"
-              />
+              <div className="row justify-content-md-center">
+                <div className="col-sm-6">
+                  <EquationVariable
+                    value={this.state.cubesRed}
+                    checkCallback={(title, checked) => {
+                      this.setState({ cubesRedChecked: true, selFactorRedChecked: false, towerSelChecked: false })
+                    }}
+                    valueCallback={(title, cubesRed) => {
+                      this.setState({ cubesRed: parseFloat(cubesRed) })
+                    }}
+                    checkable={true}
+                    checked={this.state.cubesRedChecked}
+                    min={1}
+                    label="Red cubes"
+                  />
+                </div>
+                <div className="col-sm-6">
+                  <EquationVariable
+                    value={this.state.cubesRed / this.state.cubesBlue}
+                    valueCallback={(title, percentage) => {
+                      this.setState({ cubesRed: this.state.cubesBlue * parseFloat(percentage)})
+                    }}
+                    label="Red cubes (as % of blue)"
+                  />
+                  <EquationVariable
+                    value={this.state.cubesRed / 105}
+                    valueCallback={(title, permin) => {
+                      this.setState({ cubesRed: 105 * parseFloat(permin)})
+                    }}
+                    label="Red cubes (as cubes/sec)"
+                  />
+                </div>
+              </div>
             </div>
             <div className="col-sm-6">
-              <EquationVariable
-                value={this.state.cubesBlue}
-                checkCallback={(title, checked) => {
-                  this.setState({ cubesRedChecked: true, selFactorRedChecked: false, towerSelChecked: false })
-                }}
-                valueCallback={(title, cubesBlue) => {
-                  this.setState({ cubesBlue })
-                }}
-                checkable={true}
-                checked={this.state.cubesRedChecked}
-                label="Blue cubes"
-              />
+            <div className="row justify-content-md-center">
+                <div className="col-sm-6">
+                  <EquationVariable
+                    value={this.state.cubesBlue}
+                    checkCallback={(title, checked) => {
+                      this.setState({ cubesRedChecked: true, selFactorRedChecked: false, towerSelChecked: false })
+                    }}
+                    valueCallback={(title, cubesBlue) => {
+                      this.setState({ cubesBlue: parseFloat(cubesBlue) })
+                    }}
+                    checkable={true}
+                    checked={this.state.cubesRedChecked}
+                    min={1}
+                    label="Blue cubes"
+                  />
+                </div>
+                <div className="col-sm-6">
+                  <EquationVariable
+                    value={this.state.cubesBlue / this.state.cubesRed}
+                    valueCallback={(title, percentage) => {
+                      this.setState({ cubesBlue: this.state.cubesRed * percentage})
+                    }}
+                    label="Blue cubes (as % of red)"
+                  />
+                  <EquationVariable
+                    value={this.state.cubesBlue / 105}
+                    valueCallback={(title, permin) => {
+                      this.setState({ cubesBlue: 105 * parseFloat(permin)})
+                    }}
+                    label="Blue cubes (as cubes/sec)"
+                  />
+                </div>
+              </div>
             </div>
           </div>
           <div className="row justify-content-md-center">
@@ -76,7 +131,7 @@ class App extends Component {
                   this.setState({ cubesRedChecked: false, selFactorRedChecked: true, towerSelChecked: false })
                 }}
                 valueCallback={(title, selFactorRed) => {
-                  this.setState({ selFactorRed })
+                  this.setState({ selFactorRed: parseFloat(selFactorRed) })
                 }}
                 checkable={true}
                 checked={this.state.selFactorRedChecked}
@@ -92,7 +147,7 @@ class App extends Component {
                   this.setState({ cubesRedChecked: false, selFactorRedChecked: true, towerSelChecked: false })
                 }}
                 valueCallback={(title, selFactorBlue) => {
-                  this.setState({ selFactorBlue })
+                  this.setState({ selFactorBlue: parseFloat(selFactorBlue) })
                 }}
                 checkable={true}
                 checked={this.state.selFactorRedChecked}
@@ -110,7 +165,7 @@ class App extends Component {
                   this.setState({ cubesRedChecked: false, selFactorRedChecked: false, towerSelChecked: true })
                 }}
                 valueCallback={(title, towerSel) => {
-                  this.setState({ towerSel })
+                  this.setState({ towerSel: parseFloat(towerSel) })
                 }}
                 checkable={true}
                 checked={this.state.towerSelChecked}
@@ -133,9 +188,18 @@ class App extends Component {
             <br />
             [Red points from cubes + Red points from towers] - [Blue points from cubes + Blue points from towers]
             <br />
-            [<b>CubesRed</b> + (<b>SelFactorRed</b> * (<b>CubesRed</b> * (<b>TowerCubes</b> - 1)) )] - [<b>CubesBlue</b> + (<b>SelFactorBlue</b> * (<b>CubesBlue</b> * (<b>TowerCubes</b> - 1)) )]
+            [<b>CubesRed</b> + floor(<b>SelFactorRed</b> * (<b>CubesRed</b> * <b>TowerCubes</b>) )] - [<b>CubesBlue</b> + floor(<b>SelFactorBlue</b> * (<b>CubesBlue</b> * <b>TowerCubes</b>) )]
             <br />
             [<b>{this.state.cubesRed}</b> + (<b>{this.state.selFactorRed}</b> * (<b>{this.state.cubesRed}</b> * (<b>{this.state.towerSel}</b> - 1)))] - [<b>{this.state.cubesBlue}</b> + (<b>{this.state.selFactorBlue}</b> * (<b>{this.state.cubesBlue}</b> * (<b>{this.state.towerSel}</b> - 1)))]
+            {
+              this.state.towerSel-1 < 0 ?
+              <>
+                <br/>
+                *Ignoring points from towers as tower cubes is 0
+              </>
+              :
+              <></>
+            }
             <br />
             {result}
             <br /><br />
